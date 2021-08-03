809eaa9de4d0533513a902f3a31fc46d224e5656
cloud strife <bopwerks@acm.org>
2021-08-02 20:33:36 -0400

    Makes reader support strings and adds unit tests.

--- lang.py
+++ lang.py
@@ -1,6 +1,7 @@
 import operator
 import model
 import log
+import io
 
 """
 ;; Example code for implementing a "Tag" object whose checkbox field
@@ -259,3 +260,298 @@ def make_object_filter(title, predicate):
 @builtin("delete-object-filter")
 def delete_object_filter(object_filter_id):
     model.delete_object_filter(object_filter_id)
+
+class BufferedReader(object):
+    def __init__(self, file):
+        self.file = file
+        self.buf = ""
+        self.pos = 0
+        self._eof = False
+    
+    def _fill(self):
+        if self.pos == len(self.buf):
+            self.buf = self.file.read(4096)
+            self.pos = 0
+            self._eof = not bool(self.buf)
+        return not self._eof
+    
+    def peek(self):
+        if not self._fill():
+            return ''
+        return self.buf[self.pos]
+    
+    def eof(self):
+        return not self._fill()
+    
+    def get(self):
+        if not self._fill():
+            return ''
+        ch = self.buf[self.pos]
+        self.pos += 1
+        return ch
+
+class Lexer(object):
+    def __init__(self, br):
+        self.br = br
+    
+    def match_any(self, tokens):
+        ch = self.br.get()
+        if ch not in tokens:
+            raise SyntaxError()
+    
+    def match(self, str):
+        for ch in str:
+            self.match_any(ch)
+
+whitespace = " \r\n\t"
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+digits = "0123456789"
+
+def read_comment(lexer):
+    lexer.match(";;")
+    done = False
+    while not done:
+        ch = lexer.get()
+        done = (ch == '\n')
+    return read_whitespace
+
+def read_symbol(lexer):
+    done = False
+    symbol = lexer.match(letters)
+    while not done:
+        symbol += lexer
+        ch = lexer.peek()
+        done = ch in letters or ch in numbers
+        if not done:
+            lexer.match(ch)
+
+def read_quote(lexer):
+    pass
+
+def read_number(lexer):
+    pass
+
+def read_list(lexer):
+    pass
+
+def read_string(lexer):
+    pass
+
+def read_whitespace(lexer):
+    while not lexer.eof():
+        ch = lexer.peek()
+        if ch == ';':
+            return read_comment
+        elif ch == '\'':
+            return read_quote
+        elif ch == '"':
+            return read_string
+        elif ch in letters:
+            return read_symbol
+        elif ch in numbers or ch == '.':
+            return read_number
+        elif ch == '(':
+            return read_list
+        elif ch not in whitespace:
+            raise SyntaxError()
+
+class State(object):
+    COMMENT     = 1
+    NUMBER      = 3
+    QUOTE       = 4
+    STRING      = 5
+    SYMBOL      = 6
+    WHITESPACE  = 7
+    ESCAPE      = 8
+
+def read_lexeme(br):
+    state = State.WHITESPACE
+    ch = br.peek()
+    token = ""
+    while True:
+        if state == State.WHITESPACE:
+            if br.eof():
+                return Lexeme.eof()
+            ch = br.peek()
+            if ch == '#':
+                state = State.COMMENT
+            elif ch == '\'':
+                br.get()
+                return Lexeme.quote()
+            elif ch == '"':
+                state = State.STRING
+                br.get()
+            elif ch in letters:
+                state = State.SYMBOL
+            elif ch in digits or ch == '.':
+                state = State.NUMBER
+            elif ch == '(':
+                br.get()
+                return Lexeme.left_paren()
+            elif ch == ')':
+                br.get()
+                return Lexeme.right_paren()
+            elif ch not in whitespace:
+                raise SyntaxError()
+            else:
+                br.get()
+        elif state == State.COMMENT:
+            if br.eof():
+                return Lexeme.eof()
+            ch = br.get()
+            if ch == '\n':
+                state = State.WHITESPACE
+        elif state == State.NUMBER:
+            if br.eof():
+                return Lexeme.number(token)
+            ch = br.peek()
+            if ch in digits:
+                token += br.get()
+            else:
+                return Lexeme.number(token)
+        elif state == State.STRING:
+            if br.eof():
+                return Lexeme.error("Unexpected EOF")
+            ch = br.peek()
+            if ch == '"':
+                br.get()
+                return Lexeme.string(token)
+            elif ch == '\\':
+                state = State.ESCAPE
+            else:
+                token += br.get()
+        elif state == State.ESCAPE:
+            if br.eof():
+                return Lexeme.error("Unexpected EOF")
+            token += br.get()
+            state = State.STRING
+        elif state == State.SYMBOL:
+            if br.eof():
+                return Lexeme.symbol(token)
+            ch = br.peek()
+            if ch in letters or ch in digits:
+                token += br.get()
+            else:
+                return Lexeme.symbol(token)
+
+class Lexer(object):
+    def __init__(self, br):
+        self.br = br
+        self.lexeme = read_lexeme(self.br)
+    
+    def accept(self):
+        self.lexeme = read_lexeme(self.br)
+    
+    def match(self, lexeme_type):
+        if self.lexeme.type != lexeme_type:
+            raise SyntaxError()
+        self.lexeme = read_lexeme(self.br)
+    
+    def peek(self):
+        return self.lexeme
+
+class Symbol(object):
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return self.name
+    
+    def __eq__(self, other):
+        if type(other) == str:
+            return self.name == other
+        return self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+def read_sexpr(lexer):
+    lexeme = lexer.peek()
+    typ = lexeme.type
+    if typ == Lexeme.LPAREN:
+        lexer.accept()
+        return read_list(lexer)
+    elif typ == Lexeme.SYMBOL:
+        lexer.accept()
+        return Symbol(lexeme.token)
+    elif typ == Lexeme.NUMBER:
+        lexer.accept()
+        return int(lexeme.token)
+    elif typ == Lexeme.STRING:
+        lexer.accept()
+        return lexeme.token
+    else:
+        raise SyntaxError()
+
+def read_list(lexer):
+    L = []
+    done = False
+    while not done:
+        lexeme = lexer.peek()
+        if lexeme.type == Lexeme.EOF or lexeme.type == Lexeme.ERROR or lexeme.type == Lexeme.RPAREN:
+            done = True
+        else:
+            L.append(read_sexpr(lexer))
+    lexeme = lexer.peek()
+    if lexeme.type != Lexeme.RPAREN:
+        raise SyntaxError()
+    lexer.accept()
+    return L
+
+def read(file):
+    if type(file) == str:
+        with io.StringIO(file) as fp:
+            br = BufferedReader(fp)
+            lexer = Lexer(br)
+            return read_sexpr(lexer)
+    else:
+        br = BufferedReader(file)
+        lexer = Lexer(br)
+        return read_sexpr(lexer)
+
+class Lexeme(object):
+    EOF    = 0
+    LPAREN = 1
+    NUMBER = 2
+    QUOTE  = 3
+    RPAREN = 4
+    STRING = 5
+    SYMBOL = 6
+    ERROR  = 7
+
+    def __init__(self, type, token, line=0):
+        self.type = type
+        self.token = token
+        self.line = line
+    
+    @staticmethod
+    def eof(line=0):
+        return Lexeme(Lexeme.EOF, "", line)
+    
+    @staticmethod
+    def quote(line=0):
+        return Lexeme(Lexeme.QUOTE, "'", line)
+    
+    @staticmethod
+    def left_paren(line=0):
+        return Lexeme(Lexeme.LPAREN, "(", line)
+    
+    @staticmethod
+    def right_paren(line=0):
+        return Lexeme(Lexeme.RPAREN, ")", line)
+    
+    @staticmethod
+    def number(token, line=0):
+        return Lexeme(Lexeme.NUMBER, token, line)
+    
+    @staticmethod
+    def string(token, line=0):
+        return Lexeme(Lexeme.STRING, token, line)
+    
+    @staticmethod
+    def symbol(token, line=0):
+        return Lexeme(Lexeme.SYMBOL, token, line)
+    
+    @staticmethod
+    def error(message, line=0):
+        return Lexeme(Lexeme.ERROR, message, line)
---
+++ test_lang.py
@@ -0,0 +1,179 @@
+from nose.tools import *
+from lang import *
+import io
+
+# BufferedReader
+
+def test_buffered_reader_input_empty_is_eof():
+    with io.StringIO("") as fp:
+        br = BufferedReader(fp)
+        assert br.eof() == True
+
+def test_buffered_reader_input_nonempty_is_not_eof():
+    with io.StringIO("c") as fp:
+        br = BufferedReader(fp)
+        assert br.eof() == False
+
+def test_buffered_reader_input_empty_peek():
+    with io.StringIO("") as fp:
+        br = BufferedReader(fp)
+        assert br.peek() == ''
+
+def test_buffered_reader_input_nonempty_peek():
+    with io.StringIO("c") as fp:
+        br = BufferedReader(fp)
+        assert br.peek() == 'c'
+
+def test_buffered_reader_peek_does_not_consume():
+    with io.StringIO("cd") as fp:
+        br = BufferedReader(fp)
+        br.peek()
+        assert br.peek() == 'c'
+
+def test_buffered_reader_input_empty_get():
+    with io.StringIO("") as fp:
+        br = BufferedReader(fp)
+        assert br.get() == ''
+
+def test_buffered_reader_input_nonempty_get():
+    with io.StringIO("c") as fp:
+        br = BufferedReader(fp)
+        assert br.get() == 'c'
+
+def test_buffered_reader_get_consumes():
+    with io.StringIO("cd") as fp:
+        br = BufferedReader(fp)
+        br.get()
+        assert br.get() == 'd'
+
+# TODO: Lexer
+
+def test_read_lexeme_input_whitespace_only():
+    with io.StringIO(" \t\n# hello world\n\t\n ") as fp:
+        br = BufferedReader(fp)
+        lexeme = read_lexeme(br)
+        assert lexeme.type == Lexeme.EOF
+
+def test_read_lexeme_left_paren():
+    with io.StringIO("(") as fp:
+        br = BufferedReader(fp)
+        lexeme = read_lexeme(br)
+        assert lexeme.type == Lexeme.LPAREN
+
+def test_read_lexeme_right_paren():
+    with io.StringIO(")") as fp:
+        br = BufferedReader(fp)
+        lexeme = read_lexeme(br)
+        assert lexeme.type == Lexeme.RPAREN
+
+def test_read_lexeme_right_paren():
+    with io.StringIO(")") as fp:
+        br = BufferedReader(fp)
+        lexeme = read_lexeme(br)
+        assert lexeme.type == Lexeme.RPAREN
+
+def test_read_lexeme_number():
+    with io.StringIO("1234") as fp:
+        br = BufferedReader(fp)
+        lexeme = read_lexeme(br)
+        assert lexeme.type == Lexeme.NUMBER
+
+def test_read_lexeme_string_type():
+    with io.StringIO('"hello world"') as fp:
+        br = BufferedReader(fp)
+        lexeme = read_lexeme(br)
+        assert lexeme.type == Lexeme.STRING
+
+def test_read_lexeme_string_token():
+    with io.StringIO('"hello world"') as fp:
+        br = BufferedReader(fp)
+        lexeme = read_lexeme(br)
+        assert lexeme.token == "hello world"
+
+def test_read_lexeme_string():
+    with io.StringIO('"hello world"') as fp:
+        br = BufferedReader(fp)
+        lexeme = read_lexeme(br)
+        assert lexeme.token == "hello world"
+
+def test_read_lexeme_symbol_type():
+    with io.StringIO('hello123') as fp:
+        br = BufferedReader(fp)
+        lexeme = read_lexeme(br)
+        assert lexeme.type == Lexeme.SYMBOL
+
+def test_read_lexeme_symbol_token():
+    with io.StringIO('hello123') as fp:
+        br = BufferedReader(fp)
+        lexeme = read_lexeme(br)
+        assert lexeme.token == "hello123"
+
+def test_read_lexeme_quote():
+    with io.StringIO("'") as fp:
+        br = BufferedReader(fp)
+        lexeme = read_lexeme(br)
+        assert lexeme.type == Lexeme.QUOTE
+
+# TODO: Reader
+
+def test_read_string():
+    with io.StringIO('"hello world"') as fp:
+        br = BufferedReader(fp)
+        lexer = Lexer(br)
+        expr = read_sexpr(lexer)
+        assert expr == "hello world"
+
+def test_read_number():
+    with io.StringIO("1234") as fp:
+        br = BufferedReader(fp)
+        lexer = Lexer(br)
+        expr = read_sexpr(lexer)
+        assert expr == 1234
+
+def test_read_symbol_type():
+    with io.StringIO("hello1234") as fp:
+        br = BufferedReader(fp)
+        lexer = Lexer(br)
+        expr = read_sexpr(lexer)
+        assert type(expr) == Symbol
+        assert expr.name == "hello1234"
+
+def test_read_symbol_name():
+    with io.StringIO("hello1234") as fp:
+        br = BufferedReader(fp)
+        lexer = Lexer(br)
+        expr = read_sexpr(lexer)
+        assert expr.name == "hello1234"
+
+def test_read_empty_list():
+    with io.StringIO('()') as fp:
+        br = BufferedReader(fp)
+        lexer = Lexer(br)
+        expr = read_sexpr(lexer)
+        assert expr == []
+
+def test_read_list_one_elt():
+    with io.StringIO('(3)') as fp:
+        br = BufferedReader(fp)
+        lexer = Lexer(br)
+        expr = read_sexpr(lexer)
+        assert expr == [3]
+
+def test_read_list_multi_elt():
+    with io.StringIO('(3 "hello world" )') as fp:
+        br = BufferedReader(fp)
+        lexer = Lexer(br)
+        expr = read_sexpr(lexer)
+        assert expr == [3, "hello world"]
+
+def test_read_list_nested_elt():
+    with io.StringIO('(3 "hello world" (hello 123))') as fp:
+        br = BufferedReader(fp)
+        lexer = Lexer(br)
+        expr = read_sexpr(lexer)
+        assert expr == [3, "hello world", [Symbol("hello"), 123]]
+
+# Evaluator
+
+def test_and():
+    assert eval(read("(and 1 2 3 0)")) == False
